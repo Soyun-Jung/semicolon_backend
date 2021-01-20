c183400dedf1002c8b90e3c2cacce3fe414da621
Soyun <flzlfkdrnfma@naver.com>
2021-01-20 20:46:01 +0900

    Edition N2.

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -31,5 +31,13 @@ export default {
         })
         .aggregate()
         .count()
+    ,
+    commentCount: parent =>
+      prisma
+        .commentsConnection({
+          where: { post: { id: parent.id } }
+        })
+        .aggregate()
+        .count()
   }
 };
--- src/api/User/User.js
+++ src/api/User/User.js
@@ -1,34 +1,56 @@
 import { prisma } from "../../../generated/prisma-client";
 
 export default {
-  User: {
-    fullName: parent => {
-      return `${parent.firstName} ${parent.lastName}`;
-    },
-    isFollowing: async (parent, _, { request }) => {
-      const { user } = request;
-      const { id: parentId } = parent;
-      try {
-        return prisma.$exists.user({
-          AND: [
-            {
-              id: user.id
-            },
-            {
-              following_some: {
-                id: parentId
-              }
+    User: {
+        posts: ({ id }) => prisma.user({ id }).posts(),
+        following: ({ id }) => prisma.user({ id }).following(),
+        followers: ({ id }) => prisma.user({ id }).followers(),
+        likes: ({ id }) => prisma.user({ id }).likes(),
+        comments: ({ id }) => prisma.user({ id }).comments(),
+        rooms: ({ id }) => prisma.user({ id }).rooms(),
+        postsCount: ({ id }) =>
+            prisma
+                .postsConnection({ where: { user: { id } } })
+                .aggregate()
+                .count(),
+        followingCount: ({ id }) =>
+            prisma
+                .usersConnection({ where: { followers_some: { id } } })
+                .aggregate()
+                .count(),
+        followersCount: ({ username }) => {
+            console.log(username)
+            return prisma
+                .usersConnection({ where: { following_some: { username } } })
+                .aggregate()
+                .count()
+        },
+
+        fullName: parent => `${parent.firstName} ${parent.lastName}`,
+        isFollowing: async (parent, _, { request }) => {
+            const { user } = request;
+            const { id: parentId } = parent;
+            try {
+                return prisma.$exists.user({
+                    AND: [
+                        {
+                            id: user.id
+                        },
+                        {
+                            following_some: {
+                                id: parentId
+                            }
+                        }
+                    ]
+                });
+            } catch {
+                return false;
             }
-          ]
-        });
-      } catch {
-        return false;
-      }
-    },
-    isSelf: (parent, _, { request }) => {
-      const { user } = request;
-      const { id: parentId } = parent;
-      return user.id === parentId;
+        },
+        isSelf: (parent, _, { request }) => {
+            const { user } = request;
+            const { id: parentId } = parent;
+            return user.id === parentId;
+        }
     }
-  }
 };
