aa8124f1b2d136382bf88174a15445bc89ce9799
Soyun <flzlfkdrnfma@naver.com>
2021-02-05 09:23:03 +0900

    Stroy

--- src/api/Story/Story.js
+++ src/api/Story/Story.js
@@ -4,7 +4,7 @@ export default {
   Story: {
     files: ({ id }) => prisma.story({ id }).files(),
     user: ({ id }) => prisma.story({ id }).user(),
-    seenUsers: ({ id }) => prisma.story({ id }).user(),
-    tagUser: ({ id }) => prisma.story({ id }).user(),
+    seenUsers: ({ id }) => prisma.story({ id }).seenUsers(),
+    tagUser: ({ id }) => prisma.story({ id }).tagUser(),
   }
 };
