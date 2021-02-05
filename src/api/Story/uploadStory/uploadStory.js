6110a62fefe54c1ae19e22a6273530327756420c
Soyun <flzlfkdrnfma@naver.com>
2021-02-05 13:32:36 +0900

    story #2

--- src/api/Story/uploadStory/uploadStory.js
+++ src/api/Story/uploadStory/uploadStory.js
@@ -3,34 +3,67 @@ import { isAuthenticated } from "../../../middlewares";
 
 export default {
     Mutation: {
-        upload: async (_, args, { request }) => {
+        uploadStory: async (_, args, { request }) => {
             isAuthenticated(request);
 
             const { caption, files, tagUser } = args;
             const { user } = request;
 
-            const story = await prisma.createStory({
-                tagUser: {
-                    connect: {
-                        username:tagUser
-                    }
-                },
-                caption,
-                user: {
-                    connect: {
-                        id: user.id
-                    }
+            try {
+                //태그한 사람이 있을 때
+                if (tagUser) {
+                    const [tag] = await prisma.user({ id: user.id }).following({ where: { username_in: tagUser } })
+            
+                    console.log(tag.username);
+
+                    const story = await prisma.createStory({
+                        tagUser: {
+                            connect: {
+                                username: tag.username
+                            }
+                        },
+                        caption,
+                        user: {
+                            connect: {
+                                id: user.id
+                            }
+                        }
+                    });
+                    files.forEach(async file => await prisma.createFile({
+                        url: file,
+                        story: {
+                            connect: {
+                                id: story.id
+                            }
+                        }
+                    }));
+                    return true;
                 }
-            });
-            files.forEach( async file => await prisma.createFile({
-                url: file,
-                story: {
-                    connect: {
-                        id:story.id
-                    }
+                //태그한 사람이 없을 때
+                else {
+                    const story = await prisma.createStory({
+                        caption,
+                        user: {
+                            connect: {
+                                id: user.id
+                            }
+                        }
+                    });
+                    files.forEach(async file => await prisma.createFile({
+                        url: file,
+                        story: {
+                            connect: {
+                                id: story.id
+                            }
+                        }
+                    }));
+                    return true;
                 }
-            }));
-            return story;
+            }
+            catch {
+                return false;
+             }
+            
         }
     }
 }
