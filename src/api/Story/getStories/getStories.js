c87d44347ecdcb7a2d0a6493cbe2b3f84d771793
Soyun <flzlfkdrnfma@naver.com>
2021-02-08 20:36:14 +0900

    fixed"

--- src/api/Story/getStories/getStories.js
+++ src/api/Story/getStories/getStories.js
@@ -1,10 +1,28 @@
 import { prisma } from "../../../../generated/prisma-client";
+import { isAuthenticated } from "../../../middlewares"
 
 export default {
-  Query: {
-    getStories: async (_, args) => {
-      const { id } = args;
-      return prisma.story({ id });
+    Query: {
+        getStories: (_, args, { request }) => {
+            isAuthenticated(request);
+            const { id } = args;
+        console.log(id);f
+            return prisma.stories({
+                where: {
+                    AND:
+                        [{
+                            user: {
+                                id 
+                            }
+                        },
+                        {
+                            state:'1'    
+                        }
+                    ]
+                    
+                },
+               orderBy:"createdAt_DESC"
+            });
+        }
     }
-  }
-};
+}; 
