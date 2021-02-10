922e19793e8097d3a7b56da5e28390657ede33d6
Soyun <flzlfkdrnfma@naver.com>
2021-02-10 14:40:13 +0900

    duplicate

--- src/api/Story/feedStories/feedStories.js
+++ src/api/Story/feedStories/feedStories.js
@@ -10,17 +10,25 @@ export default {
 
             const following = await prisma.user({ id: user.id }).following();
             
+            //const gotstory = await prisma.users({where:{}})
             const gotstory = await prisma.stories({ where: { user: { username_in: following.username } } }).user();
             
             gotstory.map(info => {
                 if (info.user.id !== user.id) {
-                        sliejls.push(info.user)
+                    // if (같으면) {
+                            sliejls.push(info.user)
+                        // }
                   }  
             })
-            
-            console.log(sliejls)
-            return sliejls;
-                
+            //console.log(sliejls);
+            const seen = new Set();
+            const filteredArr = sliejls.filter(el => {
+                const duplicate = seen.has(el.id);
+                     seen.add(el.id);
+                 return !duplicate;
+           });
+            console.log(filteredArr);
+            return filteredArr;
         }
     }
 }; 
