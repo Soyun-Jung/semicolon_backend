b636304783274ae8c8dacac9cbcf960f41e4d23e
Soyun Jung <73936581+Soyun-Jung@users.noreply.github.com>
2021-02-17 10:44:12 +0900

    local

--- src/recommendation.js
+++ src/recommendation.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getRecommendation = async () => { 
         try {
-            const { data } = await axios.get(`http://localhost:5000/recommendation`);
+            const { data } = await axios.get(`http://127.0.0.1:5000/recommendation`);
             return data;
         } catch (e) { 
             console.log("데이터가 존재 하지 않습니다.");
